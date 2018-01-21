8d3659204fdee538511dc37b0d6f43cc82de5df5
Verst Siu <verstsiu@126.com>
2018-01-21 18:32:05 +0800

    #add Add ConfigState.checkUpgrade, getValue, getValueIfPresent api.

--- itemplotter/src/main/kotlin/com/ijoic/item_plotter/config/ConfigState.kt
+++ itemplotter/src/main/kotlin/com/ijoic/item_plotter/config/ConfigState.kt
@@ -6,7 +6,7 @@ package com.ijoic.item_plotter.config
  * @author xiao.yl on 2018/1/21.
  * @version 1.0
  */
-class ConfigState<in VALUE>: StateItem {
+class ConfigState<VALUE>: StateItem {
 
   private var stateInit = false
   private var oldValue: VALUE? = null
@@ -14,9 +14,39 @@ class ConfigState<in VALUE>: StateItem {
 
   /**
    * Set config value.
+   *
+   * @param value config value.
    */
   fun setValue(value: VALUE) {
     newValue = value
+  }
+
+  /**
+   * Returns config change state, and upgrade config to new value.
+   *
+   * @param value config value.
+   */
+  fun checkUpgrade(value: VALUE): Boolean {
+    setValue(value)
+    val changed = isChanged()
+    upgradeValue()
+    return changed
+  }
+
+  /**
+   * Returns current config value, or default value if config value is not found.
+   *
+   * @param defaultValue default value.
+   */
+  fun getValue(defaultValue: VALUE): VALUE {
+    return newValue ?: defaultValue
+  }
+
+  /**
+   * Returns current config value, event if config value is not initialized.
+   */
+  fun getValueIfPresent(): VALUE? {
+    return newValue
   }
 
   override fun isChanged(): Boolean {
